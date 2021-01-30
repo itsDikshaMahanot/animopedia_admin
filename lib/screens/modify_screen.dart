e3ea48f7a91deeeafd7660a0c64b2f2f0f6ff1ea
Aditya Rathore <37593616+itsAdityaRathore@users.noreply.github.com>
2021-01-30 17:08:51 +0530

    Modify Screen fix

--- lib/screens/modify_screen.dart
+++ lib/screens/modify_screen.dart
@@ -41,12 +41,21 @@ class _ModifyScreenState extends State<ModifyScreen> {
                   title: Text("Tiger Oscar",
                       style: TextStyle(
                         color: Colors.white,
-                        fontSize: 16.0,
+                        fontSize: 24.0,
                       )),
-                  background: Image.network(
-                    "https://source.unsplash.com/200x200/?fish",
-                    fit: BoxFit.cover,
-                  ),
+                  background: ShaderMask(
+                      shaderCallback: (rect) {
+                        return LinearGradient(
+                                end: Alignment.topCenter,
+                                begin: Alignment.bottomCenter,
+                                colors: [Colors.black, Colors.transparent])
+                            .createShader(Rect.fromLTRB(0, 100, 0, 350));
+                      },
+                      blendMode: BlendMode.darken,
+                      child: Image.network(
+                        "https://source.unsplash.com/200x200/?fish",
+                        fit: BoxFit.cover,
+                      )),
                 ),
                 floating: false,
                 pinned: true,
@@ -54,81 +63,83 @@ class _ModifyScreenState extends State<ModifyScreen> {
               ),
             ];
           },
-          body: Container(
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Species',
+          body: SingleChildScrollView(
+            child: Container(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Species',
+                      ),
+                      textAlign: TextAlign.center,
+                      enabled: isEditable,
+                      initialValue: 'Chichlids',
                     ),
-                    textAlign: TextAlign.center,
-                    enabled: isEditable,
-                    initialValue: 'Chichlids',
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Name',
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Name',
+                      ),
+                      textAlign: TextAlign.center,
+                      enabled: isEditable,
+                      initialValue: 'Tiger Oscar',
                     ),
-                    textAlign: TextAlign.center,
-                    enabled: isEditable,
-                    initialValue: 'Tiger Oscar',
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Native',
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Native',
+                      ),
+                      textAlign: TextAlign.center,
+                      enabled: isEditable,
+                      initialValue: 'South Africa',
                     ),
-                    textAlign: TextAlign.center,
-                    enabled: isEditable,
-                    initialValue: 'South Africa',
                   ),
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-                  height: 50.0,
-                  child: GestureDetector(
-                    onTap: () {},
-                    child: Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.red,
-                          style: BorderStyle.solid,
-                          width: 2.0,
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+                    height: 50.0,
+                    child: GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.red,
+                            style: BorderStyle.solid,
+                            width: 2.0,
+                          ),
+                          color: Colors.transparent,
+                          borderRadius: BorderRadius.circular(10.0),
                         ),
-                        color: Colors.transparent,
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Center(
-                            child: Text(
-                              "DELETE",
-                              style: TextStyle(
-                                color: Colors.red,
-                                fontFamily: 'Montserrat',
-                                fontSize: 16,
-                                fontWeight: FontWeight.w900,
-                                letterSpacing: 2,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Center(
+                              child: Text(
+                                "DELETE",
+                                style: TextStyle(
+                                  color: Colors.red,
+                                  fontFamily: 'Montserrat',
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w900,
+                                  letterSpacing: 2,
+                                ),
                               ),
-                            ),
-                          )
-                        ],
+                            )
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                )
-              ],
+                  )
+                ],
+              ),
             ),
           )),
     );
